73742d007bf2991a9124f1eba6f00f186c3c92fe
AlaaSarhan9 <alaasar8917@gmail.com>
2019-09-18 21:12:30 +0300

    view images of the available cars edit

--- carRent.js
+++ carRent.js
@@ -9,6 +9,7 @@ function Car(type, color, speed, image, madeDate, addDate){
    car.image = image;
    car.rented = 0; // 1 means rented, 0 not rented
    car.setRent = setRent; // change the value of rented;
+   return car;
 }
 
 // the function is used when we rent the car.
@@ -16,15 +17,16 @@ function setRent(status) {
   this.rented = status;
 }
 
-var car1 = Car('tyota', 'white', '200km', 'image.jpg', '12/3/2015', '3/9/2019');
-var car2 = Car('porsh', 'black', '100km', 'image.jpg', '12/4/2015', '2/9/2019');
-var car3 = Car('mercedec', 'red', '50km', 'image.jpg', '12/5/2015', '4/9/2019');
-var car4 = Car('ferrari', 'yellow', '320km', 'image.jpg', '12/6/2015', '5/9/2019');
-var car5 = Car('volks wagn', 'grey', '600km', 'image.jpg', '12/7/2015', '6/9/2019');
+var car1 = Car('tyota', 'white', '200km', 'car1.jpg', '12/3/2015', '3/9/2019');
+var car2 = Car('porsh', 'black', '100km', 'car2.jpg', '12/4/2015', '2/9/2019');
+var car3 = Car('mercedec', 'red', '50km', 'car3.jpg', '12/5/2015', '4/9/2019');
+var car4 = Car('ferrari', 'yellow', '320km', 'car4.jpg', '12/6/2015', '5/9/2019');
+var car5 = Car('volks wagn', 'grey', '600km', 'car5.jpg', '12/7/2015', '6/9/2019');
 
 var cars = [car1, car2, car3, car4, car5];
 
 function getAvailableCars(carsArr){
+
 	return carsArr.filter(function(car){
       return car.rented === 0;
 	});
@@ -33,7 +35,7 @@ function getAvailableCars(carsArr){
 // loading car images to the page
 
 var availableCars = getAvailableCars(cars);
-$.each(availableCars,function(car){
-  //$('#cars-imgs').append('<img src="' + car.image + '"');
+$.each(availableCars,function(index, car){
+  $('#cars-imgs').append('<div class="car-img-info"><img src="' + car.image + '" /><a href="#">' + car.type + '</a></div>');
 });
 
